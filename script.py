6a1530ea439d0618907816b21db4e75a00b009a3
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-12-04 18:24:15 -0500

    script for class

--- script.py
+++ script.py
@@ -5,37 +5,42 @@ import re
 onto = get_ontology("OO.owx").load()
 
 for cls in onto.classes():
-    print(f"Class: {cls.name}")
     prop_list = []
     for prop in onto.properties():
         if cls in prop.domain:
-            if re.match("has_*", prop.name):
-                print(prop.equivalent_to)
-            prop_list.append(prop.name)
-        print(prop_list)
+            if re.match("is_*", prop.name):
+                pass
+            elif re.match("has_*", prop.name):
+                prop_list.append(prop.name[4:])
+            else:
+                prop_list.append(prop.name)
     for supercls in cls.is_a:
         if supercls != Thing:
-            class_content = f"class {cls.name}({supercls.name}):\n    pass\n\n"
+            class_content = f"class {cls.name}({supercls.name}):\n"
         else:
-            class_content = f"class {cls.name}:\n    pass\n\n"
-            #print(cls.properties)#if cls != Thing:
+            class_content = f"class {cls.name}:\n"
+            if not prop_list:
+                class_content += "\tpass\n\n".expandtabs(4)
+            else:
+                args = ", ".join(prop_list)
+                class_content += f"\n\tdef __init__(self, {args}):\n".expandtabs(4)
+                for prop_name in prop_list:
+                    class_content += f"\t\tself.{prop_name} = {prop_name}\n".expandtabs(4)
+                class_content += "\n"
+            print(class_content)
+            # print(cls.properties)#if cls != Thing:
             #    print(cls.get_properties())
-        
-for prop in onto.properties():
-    print(f"Property: {prop.name}")
-    for domain in prop.domain:
-        print(f"  Domain: {domain.name}")
-    #for range_ in prop.range:
+
+
+    # for range_ in prop.range:
     #    print(f"  Range: {range_.name}")
 
-        
-    #print(f"  Superclasses: {[supercls.name for supercls in cls.is_a]}")
-    #for prop in cls.get_properties():
+    # print(f"  Superclasses: {[supercls.name for supercls in cls.is_a]}")
+    # for prop in cls.get_properties():
     #    print(f"  Property: {prop.name}")
     #    for value in prop[cls]:
     #        print(f"    Value: {value}")
 
 
-        
-#with open("Python_OOP.python", "w") as file:
+# with open("Python_OOP.python", "w") as file:
 #    file.write(file_content)
