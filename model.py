a4d2d1c720bd1293db222dd4eb77ff705f8e77f9
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-12-07 23:57:36 -0500

    get individual's code

--- model.py
+++ model.py
@@ -7,8 +7,23 @@ input_dir = "1. Inputs"
 model_dir = "1. Input Models"
 model_files = glob(f"{input_dir}/{model_dir}/*.model")
 
-onto = get_ontology("Programming Language Generation Path.owl").load()
-print(onto)
+onto_dir = "2. Input Ontology"
+onto = get_ontology(f"{input_dir}/{onto_dir}/Programming Language Generation Path.owl").load()
+
+language = "Python"
+for individual in onto.individuals():
+    for cls in onto.classes():
+        if cls.name == language and cls in individual.is_a:
+            if individual.name == f"{language.lower()}_class_initializer_head":
+                class_initializer_head = individual.code
+            elif individual.name == f"{language.lower()}_class_initializer_body":
+                class_initializer_body = individual.code
+            elif individual.name == f"{language.lower()}_attribute_getting":
+                attribute_getting = individual.code
+            elif individual.name == f"{language.lower()}_attribute_settings":
+                attribute_settings = individual.code
+            elif individual.name == f"{language.lower()}_class_initializer_tail":
+                class_initializer_tail = individual.code
 
 for model_file in model_files:
     model_tree = ET.parse(model_file)
