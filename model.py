939d8f73df9033dd17e4de672459c385c8304ef9
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-12-08 01:32:48 -0500

    python class generation

--- model.py
+++ model.py
@@ -8,7 +8,9 @@ model_dir = "1. Input Models"
 model_files = glob(f"{input_dir}/{model_dir}/*.model")
 
 onto_dir = "2. Input Ontology"
-onto = get_ontology(f"{input_dir}/{onto_dir}/Programming Language Generation Path.owl").load()
+onto = get_ontology(
+    f"{input_dir}/{onto_dir}/Programming Language Generation Path.owl"
+).load()
 
 language = "Python"
 for individual in onto.individuals():
@@ -28,40 +30,36 @@ for individual in onto.individuals():
 for model_file in model_files:
     model_tree = ET.parse(model_file)
     model_root = model_tree.getroot()
-    cprint(
-        f"Root: {model_root.tag} {model_root.attrib}", "red", attrs=["dark"], end="\n"
-    )
-    cprint(
-        f"- Package name: {model_root.attrib.get('name')}".expandtabs(4),
-        "red",
-        attrs=["dark"],
-        end="\n",
-    )
+    package_name = model_root.attrib.get("name")
 
     for content in model_root.iter("contents"):
-        cprint(
-            f"\tContent: {content.tag} {content.attrib}".expandtabs(4),
-            "red",
-            attrs=["bold"],
-            end="\n",
-        )
-        cprint(
-            f"\t- Class name: {content.attrib.get('name')}".expandtabs(4),
-            "red",
-            attrs=["bold"],
-            end="\n",
-        )
+        class_name = content.attrib.get("name")
+        class_code = f"{class_initializer_head[0]}".format(
+            class_name=class_name, n="\n", t="\t"
+        ).expandtabs(4)
 
         for feature in content.iter("features"):
-            cprint(
-                f"\t\tFeature: {feature.tag} {feature.attrib}".expandtabs(4),
-                "red",
-                attrs=["blink"],
-                end="\n",
-            )
-            cprint(
-                f"\t\t- Attribute name: {feature.attrib.get('name')}".expandtabs(4),
-                "red",
-                attrs=["blink"],
-                end="\n",
-            )
+            attribute_name = feature.attrib.get("name")
+            class_code += f"{class_initializer_body[0]}".format(
+                class_name=class_name, attribute_name=attribute_name, n="\n", t="\t"
+            ).expandtabs(4)
+        class_code += "\n"
+
+        for feature in content.iter("features"):
+            attribute_name = feature.attrib.get("name")
+            class_code += f"{attribute_getting[0]}".format(
+                class_name=class_name, attribute_name=attribute_name, n="\n", t="\t"
+            ).expandtabs(4)
+
+        for feature in content.iter("features"):
+            attribute_name = feature.attrib.get("name")
+            class_code += f"{attribute_settings[0]}".format(
+                class_name=class_name, attribute_name=attribute_name, n="\n", t="\t"
+            ).expandtabs(4)
+
+        class_code += f"{class_initializer_tail[0]}".format(
+            class_name=class_name, n="\n", t="\t"
+        ).expandtabs(4)
+
+    with open(f"{package_name.lower()}.py", "w") as file:
+        file.write(class_code)
---
+++ registration.py
@@ -0,0 +1,17 @@
+class Class:
+    def __init__(self):
+        self.id = ""
+        self.name = ""
+
+    def get_id(self):
+        return self.id
+
+    def get_name(self):
+        return self.name
+
+    def set_id(self, value):
+        self.id = value
+
+    def set_name(self, value):
+        self.name = value
+
