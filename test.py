9cf7e3fccb1025eae262a883a155269138ead898
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-12-13 09:57:05 -0500

    add pyswip

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from glob import glob
 from owlready2 import *
 from termcolor import cprint
 import xml.etree.ElementTree as ET
+from pyswip import Prolog
 
 input_dir = "1. Inputs"
 model_dir = "1. Input Models"
@@ -19,10 +20,28 @@ language_dir = (
     f"{language_list.index(language.capitalize()) + 1}. {language.capitalize()} Files"
 )
 
+
+prolog = Prolog()
+
+prolog.assertz("ancestor(A, D) :- parent(A, D)")
+prolog.assertz("ancestor(A, D) :- parent(A, P), ancestor(P, D)")
+
 for prop in onto.properties():
-    if prop.name == "has_descendant":
+    if prop.name == "has_child":
         for individual in prop.get_relations():
             print(prop.name, "->", individual[0].name, "->", individual[1].name)
             print(individual[0].name, "->", individual[1].name)
+            prolog.assertz(f"parent({individual[0].name}, {individual[1].name})")
     else:
         pass
+
+for q in prolog.query("ancestor(bbb, X)"):
+    print("bbb is ancestor of", q["X"])
+
+"""
+prolog.assertz("father(michael,john)")
+prolog.assertz("father(michael,gina)")
+list(prolog.query("father(michael,X)")) == [{'X': 'john'}, {'X': 'gina'}]
+for soln in prolog.query("father(X,Y)"):
+    print(soln["X"], "is the father of", soln["Y"])
+"""
